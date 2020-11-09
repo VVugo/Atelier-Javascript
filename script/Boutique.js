ed216012cb7ad0cf7585defe2b730db8d6b7c8fe
VVugo <adrien.brest01@gmail.com>
2020-11-09 17:11:01 +0100

    WIP:Développement boutique

---
+++ script/Boutique.js
@@ -0,0 +1,11 @@
+import * as catalogue from 'data/';
+class Article {
+    constructor(name, description, image, price) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+}
+
+const json = catalogue;
